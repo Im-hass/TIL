47843a76472747ea1ead78c96c482e817e47da60
Im-Hass <huiseon4107@gmail.com>
2022-05-12 22:31:15 +0900

    코테: 백준 - java

---
+++ Coding Test/JAVA/백준/Main_10163.java
@@ -0,0 +1,70 @@
+package _220512;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 브1
+// 구현
+public class Main_10163 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int[][] map = new int[1001][1001];
+		int N = Integer.parseInt(in.readLine());
+		int[][] paper = new int[N][4];
+		
+		for (int tc = 1; tc <= N; tc++) {
+			String[] s = in.readLine().split(" ");
+			for (int i = 0; i < s.length; i++) {
+				paper[tc - 1][i] = Integer.parseInt(s[i]);
+			}
+			
+			int x1 = paper[tc - 1][0];
+			int x2 = paper[tc - 1][0] + paper[tc - 1][2];
+			int y1 = paper[tc - 1][1];
+			int y2 = paper[tc - 1][1] + paper[tc - 1][3];
+			
+			for (int x = x1; x < x2; x++) {
+				for (int y = y1; y < y2; y++) {
+					map[x][y] = tc;
+				}
+			}
+		}
+		
+		int[] res = new int[N];
+		for (int tc = 1; tc <= N; tc++) {
+			for (int i = 0; i < map.length; i++) {
+				for (int j = 0; j < map[i].length; j++) {
+					if (map[i][j] == tc) res[tc - 1]++;
+				}
+			}
+			sb.append(res[tc - 1]).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
+
+// 한 줄에 띄어쓰기로 여러개 들어오고 int로 변환할 때 StringTokenizer를 쓰면 하나씩 받을 수 있어서 편하다.
+
+// INPUT :
+//2
+//0 0 10 10
+//2 2 6 6
+
+// OUTPUT :
+//64
+//36
+
+
+//INPUT2 :
+//3
+//0 2 10 10
+//7 9 8 4
+//8 4 10 6
+
+//OUTPUT2 :
+//81
+//25
+//60
---
+++ Coding Test/JAVA/백준/Main_13300.java
@@ -0,0 +1,66 @@
+package _220512;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 브2
+// 수학, 구현
+public class Main_13300 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String[] s = in.readLine().split(" ");
+		int N = Integer.parseInt(s[0]);
+		int K = Integer.parseInt(s[1]);
+		int[][] student = new int[6][2];
+		
+		for (int tc = 0; tc < N; tc++) {
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			int S = Integer.parseInt(st.nextToken());
+			int Y = Integer.parseInt(st.nextToken());
+			student[Y - 1][S]++;
+		}
+		
+		int cnt = 0;
+		for (int i = 0; i < student.length; i++) {
+			for (int j = 0; j < student[i].length; j++) {
+				cnt += Math.ceil(student[i][j]*1.0 / K);
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+}
+
+// INPUT :
+// 16 2
+// 1 1
+// 0 1
+// 1 1
+// 0 2
+// 1 2
+// 0 2
+// 0 3
+// 1 3
+// 1 4
+// 1 3
+// 1 3
+// 0 6
+// 1 5
+// 0 5
+// 1 5
+// 1 6
+
+// OUTPUT :
+//12
+
+
+// INPUT2 :
+//3 3
+//0 3
+//1 5
+//0 6
+
+// OUTPUT2 :
+//3
