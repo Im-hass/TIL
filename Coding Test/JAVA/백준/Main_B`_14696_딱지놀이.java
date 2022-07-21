11524443bc708f5b725ae4230dfec235ddc35d24
임하스 <77854486+Im-hass@users.noreply.github.com>
2022-07-21 23:24:19 +0900

    Create Main_B`_14696_딱지놀이.java

---
+++ Coding Test/JAVA/백준/Main_B`_14696_딱지놀이.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(in.readLine());
+		
+		for (int tc = 0; tc < N; tc++) {
+			StringTokenizer st = new StringTokenizer(in.readLine());
+			int M = Integer.parseInt(st.nextToken());
+			int[] A = new int[4];
+			for (int i = 0; i < M; i++) {
+				int n = Integer.parseInt(st.nextToken());
+				A[n - 1]++;
+			}
+			st = new StringTokenizer(in.readLine());
+			M = Integer.parseInt(st.nextToken());
+			int[] B = new int[4];
+			for (int i = 0; i < M; i++) {
+				int n = Integer.parseInt(st.nextToken());
+				B[n - 1]++;
+			}
+
+			if (A[3] > B[3]) sb.append("A").append("\n");
+			else if (A[3] < B[3]) sb.append("B").append("\n");
+			else if (A[3] == B[3] && A[2] > B[2]) sb.append("A").append("\n");
+			else if (A[3] == B[3] && A[2] < B[2]) sb.append("B").append("\n");
+			else if (A[2] == B[2] && A[1] > B[1]) sb.append("A").append("\n");
+			else if (A[2] == B[2] && A[1] < B[1]) sb.append("B").append("\n");
+			else if (A[1] == B[1] && A[0] > B[0]) sb.append("A").append("\n");
+			else if (A[1] == B[1] && A[0] < B[0]) sb.append("B").append("\n");
+			else sb.append("D").append("\n");
+			
+		}
+		
+		System.out.println(sb);
+	}
+}
