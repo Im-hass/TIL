809fd9ff27e92b5375e772ce1a86d8fa16a2c70f
임하스 <77854486+Im-hass@users.noreply.github.com>
2022-07-25 23:18:17 +0900

    Delete : Coding Test 폴더 -> Alogrithms 저장소로 변경

--- Coding Test/BACKJOON/11654_아스키코드.js
+++
@@ -1,8 +0,0 @@
-const filepath = process.platform === 'linux' ? '/dev/stdin' : 'Coding Test/BACKJOON/ex/11654_아스키코드.txt';
-const input = require('fs').readFileSync(filepath).toString().trim();
-
-console.log(solution(input));
-
-function solution(c) {
-  return c.charCodeAt();
-}
--- Coding Test/BACKJOON/2558_A+B-2.js
+++
@@ -1,8 +0,0 @@
-const filepath = process.platform === 'linux' ? '/dev/stdin' : '../../Coding Test/BACKJOON/ex/2558_A+B-2.txt';
-const input = require('fs').readFileSync(filepath).toString().trim().split('\n');
-
-console.log(solution(parseInt(input[0]), parseInt(input[1])));
-
-function solution(A, B) {
-  return (A + B);
-}
